b6fa96c2932188d9618ed1adb58f804d0c3544de
joseordonez <ord231329@uvg.edu.gt>
2024-10-03 10:21:27 -0600

    main

--- app/src/main/java/com/example/casino/MainActivity.kt
+++ app/src/main/java/com/example/casino/MainActivity.kt
@@ -4,6 +4,28 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.casino.ui.theme.CasinoTheme
 
 class MainActivity : ComponentActivity() {
@@ -12,8 +34,90 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             CasinoTheme {
-                AppNavigation()
+                LuckyCharmsView()
             }
         }
     }
 }
+//composable que contiene el nombre de la app y los juegos del casino
+@Composable
+fun LuckyCharmsView() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.fondo),
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.fillMaxSize()
+        )
+
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            Text(
+                text = "LUCKY CHARMS",
+                fontSize = 40.sp,
+                fontWeight = FontWeight.Bold,
+                color = Color.White,
+                modifier = Modifier.align(Alignment.CenterHorizontally)
+                    .padding(top = 100.dp)
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween,
+            ) {
+                FeatureSection(title = "LuckySpin", iconRes = R.drawable.luckyspin)
+                FeatureSection(title = "Torneo", iconRes = R.drawable.torneo)
+            }
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween,
+            ) {
+                FeatureSection(title = "Ruleta", iconRes = R.drawable.ruleta)
+                FeatureSection(title = "Estadisticas", iconRes = R.drawable.estadisticas)
+            }
+        }
+    }
+}
+
+@Composable
+fun FeatureSection(title: String, iconRes: Int) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.padding(8.dp)
+    ) {
+        Image(
+            painter = painterResource(id = iconRes),
+            contentDescription = title,
+            modifier = Modifier
+                .size(160.dp)
+                .padding(16.dp)
+        )
+        Text(
+            text = title,
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.White
+        )
+    }
+}
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    CasinoTheme {
+        LuckyCharmsView()
+    }
+}
+
+
